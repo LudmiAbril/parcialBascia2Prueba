0f92f0be553ea998a66f8b180bfd29548962efdf
LudmiAbril <ludmila.pereyra543@gmail.com>
2022-11-20 11:02:34 -0300

    modelo al usuario y le paso la central como atributo para los metodos futuros

--- src/ar/edu/unlam/pb2/Usuario.java
+++ src/ar/edu/unlam/pb2/Usuario.java
@@ -1,5 +1,40 @@
 package ar.edu.unlam.pb2;
 
 public class Usuario {
+	private Integer DNI;
+	private String nombre;
+	private Central central;
+	
+	public Usuario(Integer dni, String nombre, Central central) {
+		this.DNI = dni;
+		this.nombre = nombre;
+		this.central=central;
+	}
+
+	public Integer getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(Integer dNI) {
+		this.DNI = dNI;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Central getCentral() {
+		return central;
+	}
+
+	public void setCentral(Central central) {
+		this.central = central;
+	}
+	
+	
 
 }
