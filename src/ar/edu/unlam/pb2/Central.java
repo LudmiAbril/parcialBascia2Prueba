bf49172bc70361934adedc9f18f1dc306aacdd1e
LudmiAbril <ludmila.pereyra543@gmail.com>
2022-11-20 10:55:53 -0300

    creo la clase central,usuario y alarma para crear las listas de la central

---
+++ src/ar/edu/unlam/pb2/Central.java
@@ -0,0 +1,41 @@
+package ar.edu.unlam.pb2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Central {
+private String nombre;
+private Set <Alarma> alarmas;
+private Set <Usuario> usuarios;
+
+public Central(String nombre) {
+	this.nombre=nombre;
+	this.alarmas=new HashSet<Alarma>();
+	this.usuarios=new HashSet<Usuario>();
+}
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+public Set<Alarma> getAlarmas() {
+	return alarmas;
+}
+
+public void setAlarmas(Set<Alarma> alarmas) {
+	this.alarmas = alarmas;
+}
+
+public Set<Usuario> getUsuarios() {
+	return usuarios;
+}
+
+public void setUsuarios(Set<Usuario> usuarios) {
+	this.usuarios = usuarios;
+}
+
+}
