ebb8f5e4bb5774cf80b85d7ef09c59dbcb6bcf94
LudmiAbril <ludmila.pereyra543@gmail.com>
2022-11-20 15:42:04 -0300

    primer test

--- test/ar/edu/unlam/pb2/test.java
+++ test/ar/edu/unlam/pb2/test.java
@@ -7,8 +7,12 @@ import org.junit.Test;
 public class test {
 
 	@Test
-	public void test() {
-		fail("Prueba git");
+	public void queSePuedaAgregarUnaAlarmaAlaCentral() {
+		Central central=new Central("central-test");
+		Alarma alarma=new Alarma(123, "act123", "conf123", "alarmaBeep");
+		Usuario admin=new Administrador(22146345,"lali",central);
+		
+		assertTrue(((Administrador)admin).agregarAlarma(alarma));
 	}
 
 }
